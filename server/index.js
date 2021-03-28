39a39459462d0eb11c5e22ac4ec77db4f8e3e6c1
rameezamemon1 <rameeza.memon1@gmail.com>
2021-03-28 07:12:15 +0500

    path changed

--- server/index.js
+++ server/index.js
@@ -316,7 +316,7 @@ app.post("/knockout", async(req, res) => {
 //   });
 
 if (process.env.NODE_ENV == 'production') {
-    app.use('/static', express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, 'client/build')));
   
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')) // relative path
