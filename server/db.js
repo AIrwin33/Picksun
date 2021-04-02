32c9539bb2cca812a9b1f7d0a1c1fa97858bbe8e
Rameeza Abdul Majeed <rameeza.memon1@gmail.com>
2021-04-02 05:52:31 +0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -13,6 +13,8 @@ const proConfig = process.env.DATABASE_URL; //heroku addons
 const pool = new Pool({
   connectionString:
     process.env.NODE_ENV === "production" ? proConfig : devConfig,
+    ssl: { rejectUnauthorized: false }
 });
+
 
 module.exports = pool;
