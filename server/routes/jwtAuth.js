e84a64430d0c41527657d97fe573ee10af089b9f
Andrew Irwin <andrew.irwin33@gmail.com>
2021-04-12 09:10:57 -0500

    change format of jwt generetor

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -80,7 +80,7 @@ router.post('/login', validInfo ,async (req, res) => {
 
         //give them JWT token
 
-        const token = jwtGenerator(participant.rows[0].ExternalId__c);
+        const token = jwtGenerator(participant.rows[0].externalid__c);
         return res.json({ token });
 
     } catch(error) {
