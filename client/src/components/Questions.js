e98bcf016a38476df5c1987d056c5a917332a3f5
Andrew Irwin <andrew.irwin33@gmail.com>
2021-04-22 17:08:37 -0500

    console log opened time

--- client/src/components/Questions.js
+++ client/src/components/Questions.js
@@ -71,6 +71,7 @@ const Questions = (props) => {
 
             //if there are questions that aren't locked, then set the timing
             if(nonLockedQuestionsArr.length > 0){
+                console.log(props.contest.Opened_Time__c);
                 if(props.contest.Opened_Time__c !== null){
                     var currtime = moment();
                     var counttime = moment.duration(currtime.diff(props.contest.Opened_Time__c));
