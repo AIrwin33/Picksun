ae33a3d314907fc4452ccb35053c1b1e2300426e
Andrew Irwin <andrew.irwin33@gmail.com>
2021-04-24 20:32:39 -0500

    uncomment part answer

--- client/src/components/Question.js
+++ client/src/components/Question.js
@@ -80,23 +80,23 @@ const Question = (props) => {
         const partid = props.partsfid;
         console.log('in set answer part Id' + partid);
         const question_sfid = props.ques.sfid;
-        //const body = {partid, question_sfid, eventVal};
-        // const response = await fetch(
-        //   "/answers",
-        //   {
-        //     method: "POST",
-        //     headers: {
-        //       "Content-type": "application/json"
-        //     },
-        //     body: JSON.stringify(body)
-        //   }
-        // );
+        const body = {partid, question_sfid, eventVal};
+        const response = await fetch(
+          "/answers",
+          {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json"
+            },
+            body: JSON.stringify(body)
+          }
+        );
         
-        // const parseRes = await response.json();
-        // console.log('created part answer' + JSON.stringify(parseRes));
-        // setPartAnswer(parseRes);
-        // checkAnswer(question_sfid, eventVal, props.ques.correct_answer__c);
-        // disableQuestion(question_sfid); 
+        const parseRes = await response.json();
+        console.log('created part answer' + JSON.stringify(parseRes));
+        setPartAnswer(parseRes);
+        checkAnswer(question_sfid, eventVal, props.ques.correct_answer__c);
+        disableQuestion(question_sfid); 
         
         
       } catch (err) {
