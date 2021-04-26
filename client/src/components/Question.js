30a5cea88020bd1ae12f4653cbf0286205fed765
Andrew Irwin <andrew.irwin33@gmail.com>
2021-04-26 08:54:25 -0500

    disabled equals disabled

--- client/src/components/Question.js
+++ client/src/components/Question.js
@@ -315,13 +315,13 @@ const Question = (props) => {
         </div>
 
         <div className="btn-group m-3" role="group" aria-label="Basic example" >
-          <button type="radio" value="A" className="btn btn-primary questionButton" disabled={quest.islocked__c} onClick={handleRadioChange}>{quest.answer_a__c}</button>
-          <button type="radio" value="B" className="btn btn-primary questionButton" disabled={quest.islocked__c} onClick={handleRadioChange}>{quest.answer_b__c}</button>
+          <button type="radio" value="A" className="btn btn-primary questionButton" disabled="disabled" onClick={handleRadioChange}>{quest.answer_a__c}</button>
+          <button type="radio" value="B" className="btn btn-primary questionButton" disabled="disabled" onClick={handleRadioChange}>{quest.answer_b__c}</button>
           {quest.answer_c__c !== null &&
-            <button type="radio" value="C" className="btn btn-primary questionButton" disabled={quest.islocked__c} onClick={handleRadioChange}>{quest.answer_c__c}</button>
+            <button type="radio" value="C" className="btn btn-primary questionButton" disabled="disabled" onClick={handleRadioChange}>{quest.answer_c__c}</button>
           }
           {quest.answer_d__c !== null &&
-            <button type="radio" value="D" className="btn btn-primary questionButton" disabled={quest.islocked__c} onClick={handleRadioChange}>{quest.answer_d__c}</button>
+            <button type="radio" value="D" className="btn btn-primary questionButton" disabled="disabled" onClick={handleRadioChange}>{quest.answer_d__c}</button>
           }
 {/* 
           <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off"/>
