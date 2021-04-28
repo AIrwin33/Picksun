5911f5a9cc8873820a528e41bd09bd1b79381341
Andrew Irwin <andrew.irwin33@gmail.com>
2021-04-28 18:54:29 -0500

    remove error in resposne json

--- index.js
+++ index.js
@@ -286,7 +286,7 @@ app.get("/existingpartanswer/:partsfid/question/:questid", authorization, async(
         console.log('questionid' + questid);
         const participationExistAnswer = await pool.query("SELECT * FROM salesforce.participation_answers__c WHERE participation__c = $1 AND question__c = $2 ", [partsfid, questid]);
         console.log('existing answer' + JSON.stringify(participationExistAnswer.rows[0]));
-        res.json('existing answers' + participationExistAnswer.rows[0]);
+        res.json(participationExistAnswer.rows[0]);
     }catch(err){
         console.log('existing answer error ' + err);
     }
