c0a84034fad5f4ed47a8ec427a0a2721022b7c87
Andrew Irwin <andrew.irwin33@gmail.com>
2021-05-03 09:27:19 -0500

    timer update

--- client/src/components/Questions.js
+++ client/src/components/Questions.js
@@ -81,14 +81,14 @@ const Questions = (props) => {
             //if there are questions that aren't locked, then set the timing
             if(nonLockedQuestionsArr.length > 0){
                 console.log(props.contest.opened_timer__c);
-                if(props.contest.opened_timer__c === null){
-                  setCounter(0);
-                }
                 if(props.contest.opened_timer__c !== undefined){
                     var currtime = moment();
                     var counttime = moment.duration(currtime.diff(props.contest.opened_timer__c));
                     console.log(counttime)
                     setCounter(counttime);
+                }else if(props.contest.opened_timer__c === null){
+                  setCounter(0);
+                  console.log(counter);
                 }else{
                     var questime = props.contest.question_timer__c;
                     var millival = questime * 1000;
