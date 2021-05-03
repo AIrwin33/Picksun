4a822dce956f25a1d756b5201fdba00cf96c15eb
Andrew Irwin <andrew.irwin33@gmail.com>
2021-05-03 12:35:52 -0500

    remove syntax error

--- client/src/components/Question.js
+++ client/src/components/Question.js
@@ -302,7 +302,7 @@ const Question = (props) => {
   useEffect(() => {
     setQuest(props.ques);
     console.log('is question locked' + props.ques.islocked__c);
-    if(props.ques.islocked__c || ){
+    if(props.ques.islocked__c){
       setDisabledQuestion(true);
     }
 
