9b0b92b4516e984f99daa88ce25a1e95d4bf8297
Andrew Irwin <andrew.irwin33@gmail.com>
2021-05-03 21:17:38 -0500

    testing

--- index.js
+++ index.js
@@ -243,8 +243,8 @@ app.post("/answers", async(req, res) => {
     [expartid]
     );
         console.log(expartid);
-        console.log(participation.sfid);
-        console.log(participation);
+        console.log(participation.rows[0].sfid);
+        console.log(participation.rows);
         console.log('partid in creating answer');
       const newParticipationAnswer = await pool.query(
           "INSERT INTO salesforce.participation_answers__c (participation__c, question__c, selection__c, status__c, ExternalId__c) VALUES($1,$2,$3,$4, gen_random_uuid()) RETURNING *", 
