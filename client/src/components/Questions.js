5271d178fc2ef8bf40a565ad368b18f166b168a6
Andrew Irwin <andrew.irwin33@gmail.com>
2021-05-11 12:14:26 -0500

    change

--- client/src/components/Questions.js
+++ client/src/components/Questions.js
@@ -88,11 +88,12 @@ const Questions = (props) => {
                 console.log(closedTimerFormat);
                 console.log(currtime);
                 console.log(currtime > closedTimerFormat);
-                var counttime = moment.duration(closedTimerFormat.diff(currtime));
+                var counttime = moment.duration(currtime.diff(closedTimerFormat));
                 console.log('count time' + counttime);
 
                 if(counttime < 0){
                   disableQuestions(questionIdArr);
+                  setCounter(0);
                 }else{
                   setCounter(counttime);
                 }
