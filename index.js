07adee9694fb6d3667603525ac2bf7c82b3c91db
Andrew Irwin <andrew.irwin33@gmail.com>
2021-05-11 16:18:13 -0500

    add logic

--- index.js
+++ index.js
@@ -338,6 +338,7 @@ app.post("/clearcounter", authorization, async(req, res) => {
 
 app.get("/allendingparticipations/:contest_id", authorization, async(req, res) => {
     try{
+        const {contest_id} = req.params;
         const contestwoncount = await pool.query(
             "SELECT * FROM salesforce.participation__c WHERE contest__c = $1 AND status__c = 'Active' SORT BY wrong_answers__c ASC", 
         [contest_id]
