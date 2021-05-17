e56ff1f251a70374ff694ede2b9ddb1e73989a61
Andrew Irwin <andrew.irwin33@gmail.com>
2021-05-17 13:08:37 -0500

    testing pg promise

--- index.js
+++ index.js
@@ -399,17 +399,19 @@ app.post("/submitpartanswers", async(req, res) => {
         //check participation value
         // var values = new Inserts('${participation}, ${question},${selection}, ${selection_val}, $(exid), ${status} ', partanswers); // using Inserts as a type;
         // console.log('values' + values);
-        
+
+        console.log(partanswers);
+
 
         //participation.rows[0].sfid, question_sfid, eventVal, eventLabel, 'Submitted', gen_random_uuid()
         const cs = new pgp.helpers.ColumnSet(['?participation__c', '?question__c','selection__c', 'selection_value__c','status__c', 'ExternalId__c'], {table: {table: 'participation_answers__c', schema: 'salesforce'}});
+        console.log(cs);
+        const update = pgp.helpers.update(partanswers, cs) + 'WHERE v.Participation = t.participation AND v.Question = t.question';
+        // //=> UPDATE "fit_ratios" AS t SET "value"=v."value"
+        // //   FROM (VALUES(1,1234),(2,5678),(3,91011))
+        // //   AS v("id","value") WHERE v.id = t.id
 
-        const update = pgp.helpers.update(partanswers, cs) + 'WHERE v.participation__c = t.participation AND v. question__c = t.question';
-        //=> UPDATE "fit_ratios" AS t SET "value"=v."value"
-        //   FROM (VALUES(1,1234),(2,5678),(3,91011))
-        //   AS v("id","value") WHERE v.id = t.id
-
-        // executing the query:
+        // // executing the query:
         await db.none(update);
 
         // db.none('UPDATE salesforce.participation_answers__c SET (participation__c, question__c, selection__c, selection_value__c, status__c, ExternalId__c) VALUES $1 WHERE question__c = $2 AND participation__c = $3 RETURNING * ', values)
@@ -422,7 +424,7 @@ app.post("/submitpartanswers", async(req, res) => {
         //     });
 
     }catch(err){
-        console.log(' ' + err);
+        console.log('error on submit answer' + err);
     }
 
 });
