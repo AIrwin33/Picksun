099ad988df7628ab50a16fa7efbc64093f2ddf48
Andrew Irwin <andrew.irwin33@gmail.com>
2021-05-24 20:06:21 -0500

    remove await query

--- queries.js
+++ queries.js
@@ -43,38 +43,37 @@ const getSocketQuestions = (contest_id) => {
     });
   };
 
-  const updateSocketAnswers = (contest_id) => {
-    return new Promise((resolve) => {
-        try {
-            const {partanswers} = req.body;
+//   const updateSocketAnswers = (contest_id) => {
+//     return new Promise((resolve) => {
+//         try {
+//             const {partanswers} = req.body;
     
-            console.log(partanswers);
+//             console.log(partanswers);
     
-            const cs = new pgp.helpers.ColumnSet(['?participation__c', '?question__c','selection__c', 'selection_value__c','status__c', 'externalid__c'], {table:{table: 'participation_answers__c', schema: 'salesforce'}});
+//             const cs = new pgp.helpers.ColumnSet(['?participation__c', '?question__c','selection__c', 'selection_value__c','status__c', 'externalid__c'], {table:{table: 'participation_answers__c', schema: 'salesforce'}});
             
-            const update = pgp.helpers.update(partanswers, cs) + ' WHERE v.Participation__c = t.participation__c AND v.Question__c = t.question__c RETURNING *';
+//             const update = pgp.helpers.update(partanswers, cs) + ' WHERE v.Participation__c = t.participation__c AND v.Question__c = t.question__c RETURNING *';
     
-            // // executing the query:
-            await db.any(update)
-                .then(data => {
-                    // OK, all records have been inserted
-                    console.log('data' + data);
-                    resolve(data);
-                })
-                .catch(error => {
-                    console.log('error');
-                    // Error, no records inserted
-                });
+//             // // executing the query:
+//             await db.any(update)
+//                 .then(data => {
+//                     // OK, all records have been inserted
+//                     console.log('data' + data);
+//                     resolve(data);
+//                 })
+//                 .catch(error => {
+//                     console.log('error');
+//                     // Error, no records inserted
+//                 });
     
-        }catch(err){
-            console.log('error on submit answer' + err);
-        }
-    });
+//         }catch(err){
+//             console.log('error on submit answer' + err);
+//         }
+//     });
   };
 
   module.exports = {
     getSocketQuestions,
-    getSocketParticipation,
-    updateSocketAnswers,
+    getSocketParticipation
 
  };
