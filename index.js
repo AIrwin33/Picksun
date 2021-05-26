69499da808edfdb158aa6a9f4d59c19b32ee9edb
Andrew Irwin <andrew.irwin33@gmail.com>
2021-05-25 21:40:29 -0500

    change back to *

--- index.js
+++ index.js
@@ -472,7 +472,7 @@ app.post("/submitpartanswers", async(req, res) => {
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
   
-    app.get('/', (req, res) => {
+    app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')) // relative path
     })
   }
