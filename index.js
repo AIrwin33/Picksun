f2ed922a858c12276b49d2fffa2db1d3a127e38b
Andrew Irwin <andrew.irwin33@gmail.com>
2021-06-24 09:58:50 -0500

    add logs

--- index.js
+++ index.js
@@ -383,9 +383,10 @@ app.post("/submitpartanswers", async (req, res) => {
         const question = await pool.query("SELECT * FROM salesforce.question__c WHERE sfid = $1", [answer.question__c])
         if (question.rows[0].correct_answer__c !== answer.selection__c && question.rows[0].correct_answer__c !== null) {
             res.json(0) //incorrect
+            console.log('incorrect');
             await pool.query("UPDATE salesforce.participation__c SET wrong_answers__c = wrong_answers__c+1 WHERE sfid = $1", [answer.participation__c])
         } else {
-            
+            console.log('correct');
             res.json(1) //correct
         }
 
