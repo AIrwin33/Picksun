03907c4fe3f97441a272f9662c3c4c3893948b7b
Andrew Irwin <andrew.irwin33@gmail.com>
2021-07-28 14:28:03 -0500

    base landing

---
+++ client/src/components/Landing.js
@@ -0,0 +1,22 @@
+import React, {Component, useEffect, useState} from 'react';
+import {
+    Container,
+    Navbar,
+    Nav,
+    Image,
+    NavDropdown,
+    ResponsiveEmbed
+} from "react-bootstrap";
+
+import "./Landing.css";
+
+
+import $ from 'jquery';
+
+
+const Landing = (props) => {
+
+
+}
+
+export default Landing;
