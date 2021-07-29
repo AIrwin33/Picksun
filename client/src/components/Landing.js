3542e12558bccb3fa96b1ee351e4a0f56c62619d
Andrew Irwin <andrew.irwin33@gmail.com>
2021-07-28 20:43:07 -0500

    landing layout

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -2,11 +2,13 @@ import React, {Component, useEffect, useState} from 'react';
 import {
     Container,
     Row,
-    Col
+    Col,
+    Image
 } from "react-bootstrap";
 
 import "./Landing.css";
 
+import justLogo from  "./assets/justLogo.png";
 
 import $ from 'jquery';
 
@@ -16,14 +18,17 @@ const Landing = (props) => {
     return (
         <>
         {/* Main Body */}
-        <Container fluid className="LoginBody">
+        <Container fluid className="landingBody">
             <Row>
                 <Col>
-                    here
+                    <div>
+                        <Image src={justLogo}></Image>
+                        <h4>Welcome To PickFun</h4>
+                        <p className="text-center">Before you start Picking, let us know your location so we can notify you about free prize contests sponsored by businesses in your area.</p>
+                    </div>
                 </Col>
             </Row>
         </Container>
-        )
         </>
     )
 }
