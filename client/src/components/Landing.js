3d2cc8ec0feb4186ef9a0ed0fbd42c227c69f987
Andrew Irwin <andrew.irwin33@gmail.com>
2021-08-01 20:31:02 -0500

    landing text update

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,7 +3,8 @@ import {
     Container,
     Row,
     Col,
-    Image
+    Image,
+    Link
 } from "react-bootstrap";
 
 import "./Landing.css";
@@ -15,6 +16,11 @@ import $ from 'jquery';
 
 const Landing = (props) => {
 
+    const handleLobby = async () => {
+        console.log('handle lobby');
+
+    }
+
     return (
         <>
         {/* Main Body */}
@@ -24,8 +30,23 @@ const Landing = (props) => {
                 </Col>
                 <Col md={4} className="text-center">
                     <Image src={justLogo}></Image>
-                    <h4 className="whiteText aptifer">Welcome To PickFun</h4>
-                    <p className="whiteText proxima">Before you start Picking, let us know your location so we can notify you about free prize contests sponsored by businesses in your area.</p>
+                    <h5 className="whiteText aptifer"> How to Pickfun</h5>
+                    <p className="whiteText proxima">
+                    <span className="whiteText aptifer fontBold">Pick</span>
+                        Go to the Lobby and select a contest
+                        Simple questions about ‘what will happen next?’ are published while the live event (ex: football game) takes place
+                        Pick your answers before the timer reaches zero and then watch the live event to see if you were right
+                        Participants are ‘knocked out’ from the competition after answering a certain number of questions incorredtly. 
+                        Last player remaining wins the prize. If multiple participants survive to the contest’s end, the player with the fewest wrong answers wins. Prize is split if there’s a tie.
+                    </p>
+                    <p className="whiteText proxima">
+                    <span className="whiteText aptifer fontBold">Fun</span>
+                        Our contests cover short segements - such as one inning of a baseball game - so that fun and winning comes at you fast!
+                        No complicated scoring systems. No math skills required.
+                        You drive the action! uggest questions you want us to ask while the contest is underway.
+                        Hangout! While contests take place, come chat about whatever you want with the moderator and fellow participants.
+                    </p>
+                    <Link className="btn btn-primary" label="lets go!" to='/Lobby'>Let's Go!</Link>
                 </Col>
                 <Col md={4}>
                 </Col>
