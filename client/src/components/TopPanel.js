4e14e7e9a1f314973c5b81eb07d49cf56025c78d
Andrew Irwin <andrew.irwin33@gmail.com>
2021-08-01 21:35:21 -0500

    test menu

--- client/src/components/TopPanel.js
+++ client/src/components/TopPanel.js
@@ -43,6 +43,44 @@ const TopPanel = (props) => {
       }
     };
 
+    // Hide the contents until the burger menu finishes sliding in from the left
+      var content = document.getElementsByTagName("main")[0];
+      content.style.visibility = "hidden";
+
+      var sidebar = document.getElementsByClassName("sidebar")[0];
+
+      var lowerLayerBurger = document.getElementsByClassName("menu-toggler__line")[2];
+      lowerLayerBurger.addEventListener("animationend", function(evt) {
+        content.style.visibility = "visible";
+      });
+
+      // Add click listeners to the menu on the sidebar
+      document.getElementsByTagName("ul")[0].addEventListener("click", function(evt) {
+        // when a menu item is clicked hide the sidebar by unchecking the input
+        document.getElementById("menuToggler").checked = false;
+
+        function handleTransitionEnd(transitionEvent) {
+          
+          // show the correct content based on the target of the menu item
+          // first hide everything
+          var contentDivs = document.querySelectorAll("main div");
+          for (var i = 0; i < contentDivs.length; i++) {
+            contentDivs[i].style.display = "none";
+          }
+
+          // show the correct div
+          var contentId = evt.srcElement.hash;
+          var contentDiv = document.getElementById(contentId.substr(1));
+          contentDiv.style.display = "inherit";
+          
+          // remove listener
+          sidebar.removeEventListener("transitionend", handleTransitionEnd);
+        }
+        
+        sidebar.addEventListener("transitionend", handleTransitionEnd);
+      });
+
+
 
     useEffect(() => {
         getProfile();
@@ -60,7 +98,23 @@ const TopPanel = (props) => {
             <Navbar.Brand   className="nav-logo">
                 <img src={headerIcon} height="50"></img>
             </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" className="custom-toggler"/>
+
+            <input type="checkbox" id="menuToggler" class="input-toggler" />
+            <label for="menuToggler" class="menu-toggler">
+              <span class="menu-toggler__line"></span>
+              <span class="menu-toggler__line"></span>
+              <span class="menu-toggler__line"></span>
+            </label>
+            <aside class="sidebar">
+              <ul class="menu">
+                <li class="menu__item"><a class="menu__link" href="/">Home</a></li>
+                <li class="menu__item"><a class="menu__link" href="/Lobby">Lobby</a></li>
+                <li class="menu__item"><a class="menu__link" href="/Contests">My Contests</a></li>
+                <li class="menu__item"><a class="menu__link" href="/Profile">Profile</a></li>
+                <li class="menu__item"><a class="menu__link" href="https://pick.fun">Rules</a></li>
+              </ul>
+            </aside>
+            {/* <Navbar.Toggle aria-controls="responsive-navbar-nav" className="custom-toggler"/>
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="ml-auto">
                 <Nav.Link className="lightBlueText aptifer" href="/Lobby">Lobby</Nav.Link>
@@ -72,7 +126,7 @@ const TopPanel = (props) => {
                     <Nav.Link className="aptifer lightBlueText" href="/Login">Login</Nav.Link>
                 }  
                 </Nav>
-            </Navbar.Collapse>
+            </Navbar.Collapse> */}
         </Navbar>
     )
 }
