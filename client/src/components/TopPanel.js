aed3ff35ff6b8d5dd5c9b59adfa8c26a4f0de98d
Andrew Irwin <andrew.irwin33@gmail.com>
2021-08-02 22:43:18 -0500

    remove

--- client/src/components/TopPanel.js
+++ client/src/components/TopPanel.js
@@ -49,11 +49,6 @@ const TopPanel = (props) => {
 
       var sidebar = document.getElementsByClassName("sidebar")[0];
 
-      var lowerLayerBurger = document.getElementsByClassName("menu-toggler__line")[2];
-      lowerLayerBurger.addEventListener("animationend", function(evt) {
-        content.style.visibility = "visible";
-      });
-
       // Add click listeners to the menu on the sidebar
       document.getElementsByTagName("ul")[0].addEventListener("click", function(evt) {
         // when a menu item is clicked hide the sidebar by unchecking the input
