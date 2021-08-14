fb2f0531264bde0eec2d0742df61428fca897a11
Andrew Irwin <andrew.irwin33@gmail.com>
2021-08-13 22:29:59 -0500

    add use callback

--- client/src/components/Contest.js
+++ client/src/components/Contest.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useCallback} from 'react';
 import {Row, Col, Tab, Tabs, Image} from "react-bootstrap";
 import {TwitterTimelineEmbed} from 'react-twitter-embed';
 import {SocketContext} from "../socket";
