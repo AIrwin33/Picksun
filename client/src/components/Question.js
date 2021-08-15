d7c3a1117d1f7ce2ba8d9eed8043acac52302202
Andrew Irwin <andrew.irwin33@gmail.com>
2021-08-15 11:29:12 -0500

    font bold

--- client/src/components/Question.js
+++ client/src/components/Question.js
@@ -353,16 +353,16 @@ const Question = (props) => {
                 <Col>
                     <div className={`btn-group m-3 ${disabledQuestion === true ? "disabledBtnGroup" : ""}`} role="group"
                         aria-label="Basic example" data-toggle="buttons">
-                        <button type="radio" value="A" className="btn btn-primary questionButton font20"
+                        <button type="radio" value="A" className="btn btn-primary questionButton font20 fontBold"
                                 onClick={handleRadioChange}>{quest.answer_a__c}</button>
-                        <button type="radio" value="B" className="btn btn-primary questionButton font20"
+                        <button type="radio" value="B" className="btn btn-primary questionButton font20 fontBold"
                                 onClick={handleRadioChange}>{quest.answer_b__c}</button>
                         {quest.answer_c__c !== null &&
-                        <button type="radio" value="C" className="btn btn-primary questionButton font20"
+                        <button type="radio" value="C" className="btn btn-primary questionButton font20 fontBold"
                         onClick={handleRadioChange}>{quest.answer_c__c}</button>
                     }
                         {quest.answer_d__c !== null &&
-                        <button type="radio" value="D" className="btn btn-primary questionButton font20"
+                        <button type="radio" value="D" className="btn btn-primary questionButton font20 fontBold"
                         onClick={handleRadioChange}>{quest.answer_d__c}</button>
                     }
                     </div>
