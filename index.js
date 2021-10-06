a70e9a80c72e876bbb75a2d7235add588f4f5527
Andrew Irwin <andrew.irwin33@gmail.com>
2021-10-06 14:18:45 -0500

    remove app use express json

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 //middleware
 const cors = require("cors");
 app.use(cors());
-app.use(express.json());
+//app.use(express.json());
 const authorization = require("./server/middleware/authorize");
 const PORT = process.env.PORT || 8080;
 const path = require("path");
