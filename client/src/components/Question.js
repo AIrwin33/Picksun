75ecb538c039c1f2d8fbbef7b0afcab6d1381ec4
Andrew Irwin <andrew.irwin33@gmail.com>
2021-10-06 18:57:02 -0500

    testing

--- client/src/components/Question.js
+++ client/src/components/Question.js
@@ -308,7 +308,7 @@ const Question = (props) => {
                     
                     <div className="answerMain">
                     {allpartanswers.map(answer => {
-                        return <div className={'answerDiv ' + (answer.question__c === props.ques.sfid ? 'selected ' : '') + (answer.incorrect__c === true && answer.validated__c === true ? 'incorrect' : '')}>
+                        return <div className={'answerDiv ' + (answer.question__c === props.ques.sfid ? 'selected ' : '') + (answer.incorrect__c === true ? 'incorrect ' : '') + (answer.validated__c === true ? 'correct ' : '')}>
                         </div>
                         
                     })}
