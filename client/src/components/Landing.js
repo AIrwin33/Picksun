5f821e4b5e265626129ec8b8db9d9d72850cfc69
Andrew Irwin <andrew.irwin33@gmail.com>
2021-10-05 19:59:55 -0500

    typos front page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -24,9 +24,9 @@ const Landing = (props) => {
         {/* Main Body */}
         <Container fluid className="landingBody">
             <Row className="justify-content-md-center pt-3 pb-3">
-                <Col md={2} sm={1} lg={4}>
+                <Col md={2} sm={1} lg={2}>
                 </Col>
-                <Col md={8} sm={10} lg={4}>
+                <Col md={8} sm={10} lg={8}>
                     <Image src={justLogo}></Image>
                     <h5 className="whiteText aptifer"> How to Pickfun</h5>
                     <span className="whiteText aptifer fontBold text-center">Pick</span><br/>
@@ -34,7 +34,7 @@ const Landing = (props) => {
                     <span className="whiteText proxima">Go to the Lobby and select a contest</span><br/>
                     <span className="whiteText proxima">Simple questions about ‘what will happen next?’ are published while the live event (ex: football game) takes place</span><br/>
                     <span className="whiteText proxima">Pick your answers before the timer reaches zero and then watch the live event to see if you were right</span><br/>
-                    <span className="whiteText proxima">Participants are ‘knocked out’ from the competition after answering a certain number of questions incorredtly. </span><br/>
+                    <span className="whiteText proxima">Participants are ‘knocked out’ from the competition after answering a certain number of questions incorrectly. </span><br/>
                     <span className="whiteText proxima">Last player remaining wins the prize. If multiple participants survive to the contest’s end, the player with the fewest wrong answers wins. Prize is split if there’s a tie.</span><br/><br/><br/>
                     </div>
 
@@ -43,12 +43,12 @@ const Landing = (props) => {
                     <span className="whiteText proxima">Our contests cover short segments - such as one inning of a baseball game - so that fun and winning comes at you fast!</span><br/>
                     <span className="whiteText proxima">No complicated scoring systems. No math skills required.
                     </span><br/>
-                    <span className="whiteText proxima">You drive the action! uggest questions you want us to ask while the contest is underway.</span><br/>
+                    <span className="whiteText proxima">You drive the action! suggest questions you want us to ask while the contest is underway.</span><br/>
                     <span className="whiteText proxima">   Hangout! While contests take place, come chat about whatever you want with the moderator and fellow participants.</span><br/>
                     </div>
                     <Link className="btn btn-primary mt-3" label="lets go!" to='/Lobby'>Let's Go!</Link>
                 </Col>
-                <Col md={2} sm={1} lg={4}>
+                <Col md={2} sm={1} lg={2}>
                 </Col>
             </Row>
         </Container>
