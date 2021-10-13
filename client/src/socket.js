3fac9ddadaabc78f182f845be2d0d7b3ab9946c5
Andrew Irwin <andrew.irwin33@gmail.com>
2021-10-12 19:22:40 -0500

    add socket to html

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,5 @@
-import io from "socket.io-client";
+import {io} from "socket.io-client";
 import React from "react"
 
-export const socket = io.connect("https://cryptic-citadel-94967.herokuapp.com/", { transports: ['websocket'] });
+export const socket = io.connect("http://cryptic-citadel-94967.herokuapp.com/", { transports: ['websocket'] });
 export const SocketContext = React.createContext(null);
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 const authorization = require("./server/middleware/authorize");
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 const path = require("path");
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
