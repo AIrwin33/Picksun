9d5ab775471590fc158667a13adf9b218ad21f0c
Andrew Irwin <andrew.irwin33@gmail.com>
2021-10-19 21:03:53 -0500

    force new

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,5 @@
 import {io} from "socket.io-client";
 import React from "react"
 
-export const socket = io();
+export const socket = io({'forceNew':true});
 export const SocketContext = React.createContext(null);
