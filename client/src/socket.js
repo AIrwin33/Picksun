99e572424011c35bc2154ad961f7288201e009fa
Andrew Irwin <andrew.irwin33@gmail.com>
2021-11-30 19:57:04 -0600

    add reconnection

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,9 @@
 import {io} from "socket.io-client";
 import React from "react"
 
-export const socket = io('http://cryptic-citadel-94967.herokuapp.com/');
+export const socket = io('http://cryptic-citadel-94967.herokuapp.com/', {
+    'reconnection': true,
+    'reconnectionDelay': 500,
+    'reconnectionAttempts': 10
+  });
 export const SocketContext = React.createContext(null);
