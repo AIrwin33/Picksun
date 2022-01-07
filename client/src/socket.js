dfe2580b243ee784f4d978e1c9e3b3fab18c2927
Andrew Irwin <andrew.irwin33@gmail.com>
2022-01-06 18:37:31 -0600

    session

--- client/src/socket.js
+++ client/src/socket.js
@@ -3,8 +3,6 @@ import React from "react"
 
 export const socket = io({
   'connect timeout': 150000,
-  'reconnect': true,
-  'reconnection delay': 300,
   'max reconnection attempts': 10000
   });
 export const SocketContext = React.createContext(null);
