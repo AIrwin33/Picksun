8a63358b8423c82089a664ed83ea26419b1e1029
Andrew Irwin <andrew.irwin33@gmail.com>
2022-01-14 09:31:59 -0600

    try adding back in recon

--- client/src/socket.js
+++ client/src/socket.js
@@ -4,7 +4,11 @@ import { v4 as uuidv4 } from 'uuid';
 
 export const socket = io({
     query: {conid:uuidv4()}
-  });
+  },
+  'connect timeout': 150000,
+  'reconnection': true,
+  'max reconnection attempts': 10000,
+);
 
   // io.engine.generateId = req => {
 //     console.log('generate id');
