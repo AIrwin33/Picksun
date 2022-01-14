d794875f58c50ad1de176f46ebbdc2743f571af2
Andrew Irwin <andrew.irwin33@gmail.com>
2022-01-13 23:19:39 -0600

    var socket

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,7 +2,7 @@ import {io} from "socket.io-client";
 import React from "react";
 import { v4 as uuidv4 } from 'uuid';
 
-export const socket = io({
+var socket = io({
   'connect timeout': 150000,
   'reconnection': true,
   'max reconnection attempts': 10000,
