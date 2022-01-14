9e28ca4c1a5b943b77e9376084435894e4aba57c
Andrew Irwin <andrew.irwin33@gmail.com>
2022-01-14 09:16:21 -0600

    show

--- client/src/socket.js
+++ client/src/socket.js
@@ -3,18 +3,15 @@ import React from "react";
 import { v4 as uuidv4 } from 'uuid';
 
 export const socket = io({
-  'connect timeout': 150000,
-  'reconnection': true,
-  'max reconnection attempts': 10000,
-  
-
-  },
-  query: {
-    {foo: 'bar'}
+    query: {foo:'bar'}
   });
 
   // io.engine.generateId = req => {
 //     console.log('generate id');
 //     return uuid.v4();
 // );
+
+// 'connect timeout': 150000,
+//   'reconnection': true,
+//   'max reconnection attempts': 10000,
 export const SocketContext = React.createContext(null);
