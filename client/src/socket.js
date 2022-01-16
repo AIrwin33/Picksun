13f2cefccbe2c71fff45616ab29e6aef82c7912f
Andrew Irwin <andrew.irwin33@gmail.com>
2022-01-16 10:43:09 -0600

    test clients by room

--- client/src/socket.js
+++ client/src/socket.js
@@ -3,11 +3,9 @@ import React from "react";
 import { v4 as uuidv4 } from 'uuid';
 
 export const socket = io({
-    query: {conid:uuidv4()}
-  // },
-  // 'connect timeout': 150000,
-  // 'reconnection': true,
-  // 'max reconnection attempts': 10000,
+  'connect timeout': 150000,
+  'reconnection': true,
+  'max reconnection attempts': 10000,
 });
 
 export const SocketContext = React.createContext(null);
--- index.js
+++ index.js
@@ -458,7 +458,21 @@ pgListen.listenTo("new_contest")
 io.on("connection", (socket) => {
     console.log('connect to socket');
     console.log(socket.id);
-    console.log(socket.handshake.query.conid);
+    socket.join('testroom');
+
+    var clients = io.sockets.adapter.rooms['testroom'].sockets;   
+    console.log('clients' + clients);
+    //to get the number of clients
+    var numClients = (typeof clients !== 'undefined') ? Object.keys(clients).length : 0;
+    console.log('num of clients' + numClients);
+    for (var clientId in clients ) {
+
+        //this is the socket of each client in the room.
+        var clientSocket = io.sockets.connected[clientId];
+
+        console.log('this socket' + clientSocket);
+
+    }
    
     socket.on("set_contest_room", e => {
         console.log('set contest room' + e);
