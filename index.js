a9dbae37b1542b56863284b7a1a168dd7f80011e
Andrew Irwin <andrew.irwin33@gmail.com>
2022-01-16 13:11:53 -0600

    this plus

--- index.js
+++ index.js
@@ -431,7 +431,8 @@ pgListen.notifications.on("new_question", e => {
 
     if (e !== undefined && e.published__c && !e.islocked__c) {
         console.log('send socket question');
-        const sockets = io.in('a00170000085kfoAAA').fetchSockets();
+
+        const sockets = io.in(e.contest__c).fetchSockets();
         console.log(sockets);
         for(let soc in sockets){
             console.log(soc.id);
@@ -442,7 +443,7 @@ pgListen.notifications.on("new_question", e => {
     if(e.correct_answer__c !== null && e !== undefined) {
         console.log('e.correct_answer__c' + e.correct_answer__c);
         console.log('here');
-        const sockets = io.in('a00170000085kfoAAA').fetchSockets();
+        const sockets = io.in(e.contest__c).fetchSockets();
         console.log(sockets);
         for(let soc in sockets){
             console.log(soc.id);
