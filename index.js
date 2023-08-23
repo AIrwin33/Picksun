7a0f0be40f4aa855bf3d6996cbeb4d2838b11cdc
Andrew Irwin <andrew.irwin33@gmail.com>
2023-08-23 13:35:19 -0500

    define route

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const router = express.Router();
 const app = express();
 const http = require('http').createServer(app);
 const setupSocketIO = require('./socket');
