987b5f81f778bf0bd0ee6aed8ff445ebce3c2ee8
Andrew Irwin <andrew.irwin33@gmail.com>
2023-10-17 20:48:48 -0500

    apex fix

--- index.js
+++ index.js
@@ -355,7 +355,7 @@ app.post("/markcorrect", authorization, async (req, res) => {
                         participantid = finishedparts.rows[i].participant__c;
                         console.log('part:::' + participantid);
                         var winval;
-                        const contestswon = await pool.query("SELECT * FROM salesforce.participant__c WHERE Id = $1", [participantid]).contests_won__c;
+                        const contestswon = await pool.query("SELECT * FROM salesforce.participant__c WHERE sfid = $1", [participantid]).contests_won__c;
                         if(contestswon == null){
                             winval = 0;
                         }
