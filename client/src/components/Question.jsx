caedda37b774bca3663c31758cbd8c814ceed51b
Andrew Irwin <andrew.irwin33@gmail.com>
2024-01-08 19:51:36 -0600

    is admin use effect

--- client/src/components/Question.jsx
+++ client/src/components/Question.jsx
@@ -186,11 +186,15 @@ const Question = props => {
     console.log(props.ques);
     handleThisPartAnswer();
     updateAllPartAnswers();
+    
+  }, [props.ques.correct_answer__c, props.questionNum, props.updateanswers]);
+
+  useEffect(() => {
     console.log('templ aswer list' + JSON.stringify(props.tempanswerlist));
     if(props.tempanswerlist.length > 0 && !isAdmin && props.tempanswerlist != null){
       updateTempSelectedAnswer();
     }
-  }, [props.ques.correct_answer__c, props.questionNum, props.updateanswers, props.tempanswerlist]);
+  }, [props.tempanswerlist]);
 
   return (
     <>
