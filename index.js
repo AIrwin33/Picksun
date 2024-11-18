af93d25270a67cf77ddfbbf5a287b7f8f5242f84
techwebdev <amit.kadivar3@gmail.com>
2024-11-19 02:59:44 +0530

    fix issue

--- index.js
+++ index.js
@@ -577,10 +577,10 @@ app.post("/publishcontest", authorization, async (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
   // app.use(express.static('client/public'));
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname + "/client/build/index.html"));
+    res.sendFile(path.resolve(__dirname , "client/build","index.html"));
     // res.sendFile(path.join(__dirname, 'client','index.html')) // relative path
   });
 } else {
