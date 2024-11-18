69ce93e0f43765fa01f2608b6bbe876c63969935
techwebdev <amit.kadivar3@gmail.com>
2024-11-19 02:29:38 +0530

    fix issue

--- Picksun-testsocket/client/src/main.jsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import { Fragment, useEffect, useState } from 'react'
-import './index.css'
-import Application from './App.jsx'
-
-createRoot(document.getElementById('root')).render(<Application />)
