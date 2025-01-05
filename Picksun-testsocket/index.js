233da1f6bb62f411f33e77e6b48071a3a5732659
Andrew Irwin <andrew.irwin33@gmail.com>
2025-01-04 20:54:06 -0600

    Update index.js

--- Picksun-testsocket/index.js
+++ Picksun-testsocket/index.js
@@ -13,8 +13,13 @@ const publicUrl = env === 'development' ? 'http://localhost:3000' : process.env.
 const io = new Server(server, {
     cors: {
         origin: publicUrl,
+        origin: [
+            publicUrl || 'https://play.pick.fun',
+            'https://cryptic-citadel-94967.herokuapp.com'
+        ],
     }
 });
+
 
 
 
